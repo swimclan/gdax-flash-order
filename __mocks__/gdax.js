05daae8e6cc57c9ecdae16b90e5fc7b0d4eaeca7
Matthew Herron <matthew.herron77@gmail.com>
2018-05-29 20:28:59 -0500

    Updated mock module for gdax to use ES6 class instead of ES5 constructor functions (#7)

--- __mocks__/gdax.js
+++ __mocks__/gdax.js
@@ -6,17 +6,19 @@ const {whilst} = require('async');
 
 let orders = ['68e6a28f-ae28-4788-8d4f-5ab4e5e5ae08', 'd0c5340b-6d6c-49d9-b567-48c4bfca13d2'];
 
-Gdax.AuthenticatedClient = function(key, secret, passphrase, apiURI, options = {}) {
-  this.key = key;
-  this.secret = secret;
-  this.passphrase = passphrase;
-  this.connected = true;
-
-  this._connection = function(state) {
-    return this.connected = state === true;
+class AuthenticatedClient {
+  constructor(key, secret, passphrase, apiURI, options = {}) {
+    this.key = key;
+    this.secret = secret;
+    this.passphrase = passphrase;
+    this.connected = true;
+  
+    this._connection = function(state) {
+      return this.connected = state === true;
+    }
   }
 
-  this.getProducts = function(callback) {
+  getProducts(callback) {
     const error = !this.connected ? { message: 'An error occured' } : null;
     return callback(error, {},
       [
@@ -55,7 +57,7 @@ Gdax.AuthenticatedClient = function(key, secret, passphrase, apiURI, options = {
     ]);
   }
 
-  this.getOrder = function(orderId, callback) {
+  getOrder(orderId, callback) {
     if (orderId !== '68e6a28f-ae28-4788-8d4f-5ab4e5e5ae08') {
       return callback({message: 'Invalid orderId supplied.  Order not found.'}, {}, null);
     }
@@ -80,7 +82,7 @@ Gdax.AuthenticatedClient = function(key, secret, passphrase, apiURI, options = {
     });
   }
 
-  this.cancelOrder = function(orderId, callback) {
+  cancelOrder(orderId, callback) {
     const targetOrder = orders.indexOf(orderId);
     if (targetOrder === -1) {
       return callback({message: 'Invalid orderId supplied.  Order not found.'}, {}, null);
@@ -89,7 +91,7 @@ Gdax.AuthenticatedClient = function(key, secret, passphrase, apiURI, options = {
     return callback(null, {}, orders.splice(targetOrder, 1));
   }
 
-  this.placeOrder = function(params, callback) {
+  placeOrder(params, callback) {
     if (!has(params, 'product_id') || !has(params, 'size') || !has(params, 'side')) {
       return callback({message: 'Invalid params supplied.  Must supply size, side, product_id and price'}, {}, null);
     }
@@ -141,44 +143,49 @@ Gdax.AuthenticatedClient = function(key, secret, passphrase, apiURI, options = {
   }
 }
 
-Gdax.WebsocketClient = function(products=[], uri='wss://test.mock.com', credentials={}, options={}) {
-  this.auth = credentials
-  this.socket = {readyState: 0}
-  this.channels = get(options, 'channels', []).concat(['heartbeat']);
-  this.products = products;
+class WebsocketClient extends EventEmitter {
+  constructor(products=[], uri='wss://test.mock.com', credentials={}, options={}) {
+    super();
+    this.auth = credentials
+    this.socket = {readyState: 0}
+    this.channels = get(options, 'channels', []).concat(['heartbeat']);
+    this.products = products;
+    this.broadcastSocket();
+  }
 
-  let tradeId = 100000000;
-  // emit fake test messages every 600ms
-  whilst(() => tradeId < 100000010, (cb) => {
-    tradeId++;
-    this.emit('message', { type: 'ticker',
-    sequence: 5560214656,
-    product_id: this.products[0],
-    price: '7155.80000000',
-    open_24h: '6604.33000000',
-    volume_24h: '18682.08960534',
-    low_24h: '7155.80000000',
-    high_24h: '7200.00000000',
-    volume_30d: '592512.25034351',
-    best_bid: '7155.14',
-    best_ask: '7155.8',
-    side: 'buy',
-    time: '2018-03-31T16:53:07.051000Z',
-    trade_id: tradeId,
-    last_size: '0.10405984' });
-    setTimeout(() => cb(null, tradeId), 600)
-  }, (err) => { return; } );
-
-  // emit fake test heartbeats every 1s
-  whilst(() => tradeId < 100000010, (cb) => {
-    this.emit('heartbeat', { type: 'heartbeat',
-    last_trade_id: tradeId,
-    product_id: 'BTC-USD',
-    sequence: 5560338726,
-    time: new Date().toISOString() });
-    setTimeout(cb, 1000);
-  }, (err) => { return; } );
+  broadcastSocket() {
+    let tradeId = 100000000;
+    // emit fake test messages every 600ms
+    whilst(() => tradeId < 100000010, (cb) => {
+      tradeId++;
+      this.emit('message', { type: 'ticker',
+      sequence: 5560214656,
+      product_id: this.products[0],
+      price: '715.80000000',
+      open_24h: '660.33000000',
+      volume_24h: '18682.08960534',
+      low_24h: '715.80000000',
+      high_24h: '720.00000000',
+      volume_30d: '592512.25034351',
+      best_bid: '715.14',
+      best_ask: '715.8',
+      side: 'buy',
+      time: '2018-03-31T16:53:07.051000Z',
+      trade_id: tradeId,
+      last_size: '0.10405984' });
+      setTimeout(() => cb(null, tradeId), 200)
+    }, (err) => { return; } );
+  
+    // emit fake test heartbeats every 1s
+    whilst(() => tradeId < 100000010, (cb) => {
+      this.emit('heartbeat', { type: 'heartbeat',
+      last_trade_id: tradeId,
+      product_id: this.products[0],
+      sequence: 5560338726,
+      time: new Date().toISOString() });
+      setTimeout(cb, 1000);
+    }, (err) => { return; } );
+  }
 }
-Gdax.WebsocketClient.prototype = new EventEmitter();
 
-module.exports = Gdax;
+module.exports = { AuthenticatedClient, WebsocketClient };
