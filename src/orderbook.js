92a4bbb022206d7ae439167f0f0ad0b861a4491a
jtham <jtham@gogoair.com>
2018-05-29 22:18:34 -0500

    updated with 100% coverage

--- src/orderbook.js
+++ src/orderbook.js
@@ -1,18 +1,31 @@
-const { EventEmitter } = require('events');
-const Feeds = require('./feeds');
-
 /**
  * A class representing the level 1 order book of the exchange
  */
-class Orderbook extends EventEmitter {
+class Orderbook {
   /**
-   * 
-   * @param {Feeds} feeds - The collection of socket feeds for each product in the current orderbook
+   * @constructor
+   * @param {String} product - The product to track orders for
+   * @returns {void}
    */
-  constructor(feeds = new Feeds()) {
-    super(feeds);
-    this.feeds = feeds;
-    this.book = { bids: [], asks: [] };
+  constructor(product) {
+    this.product = product;
+    this.book = { bid: 0, ask: 0 };
+  }
+  /**
+   * Method to update the order book best bid and ask
+   * @param {Number} bid The best bid price
+   * @param {Number} ask The best ask price
+   * @memberof Orderbook
+   * @returns {void}
+   */
+  update({ bid = null, ask = null }) {
+    if (bid !== null && typeof bid !== 'number') {
+      throw new TypeError('Bid parameter is not a number');
+    } else if (ask !== null && typeof ask !== 'number') {
+      throw new TypeError('Ask parameter is not a number');
+    }
+    bid !== null && (this.book.bid = bid);
+    ask !== null && (this.book.ask = ask);
   }
 }
 
--- tests/orderbook.test.js
+++ tests/orderbook.test.js
@@ -1,17 +1,61 @@
 const Orderbook = require('../src/orderbook');
-const Feeds = require('../src/feeds');
 
-describe('Test Orderbook construction', () => {
+describe('Orderbook constructor... ', () => {
+  let orderbook;
+  afterEach(() => {
+    // reset orderbook
+    orderbook = undefined;
+  });
+  test('creates an Orderbook instance with product and book properties.', () => {
+    const ethereum = 'ETH-USD';
+    orderbook = new Orderbook(ethereum);
+    expect(orderbook).toHaveProperty('product', ethereum);
+    expect(orderbook).toHaveProperty('book', { bid: 0, ask: 0});
+  });
+
+  test('sets the product property to the string passed to the constructor.', () => {
+    const bitcoin = 'BTC-USD';
+    orderbook = new Orderbook(bitcoin);
+    expect(typeof orderbook.product).toBe('string');
+    expect(orderbook.product).toBe(bitcoin);
+  });
+});
+
+describe('Orderbook update method... ', () => {
   let orderbook;
   beforeEach(() => {
-    orderbook = new Orderbook();
-  })
-  test('new Orderbook constructed without any params will set an empty feeds instance to its feeds prop', () => {
-    expect(orderbook.feeds instanceof Feeds).toBe(true);
+    orderbook = new Orderbook('BTC-USD');
   });
 
-  test('new Orderbook instance will have feeds and book properties', () => {
-    expect(orderbook).toHaveProperty('feeds');
-    expect(orderbook).toHaveProperty('book', { bids: [], asks: [] });
+  test('takes an object with bid and ask properties as a parameter and updates the orderbook.', () => {
+    const params = { bid: 12, ask: 15 };
+    orderbook.update(params);
+    expect(orderbook.book).toEqual(params);
   });
+
+  test('takes an object with only a bid property as a parameter and updates the orderbook.', () => {
+    const params = { bid: 12 };
+    orderbook.update(params);
+    expect(orderbook.book).toEqual({ bid: 12, ask: 0 });
+  });
+
+  test('takes an object with only an ask property as a parameter and updates the orderbook.', () => {
+    const params = { ask: 12 };
+    orderbook.update(params);
+    expect(orderbook.book).toEqual({ bid: 0, ask: 12 });
+  });
+
+  test('throws an error if ask is not a number', () => {
+    expect(() => {
+      orderbook.update({ ask: 'string' });
+    })
+    .toThrowError(new TypeError('Ask parameter is not a number'));
+  })
+
+  test('throws an error if bid is not a number', () => {
+    expect(() => {
+      orderbook.update({ bid: 'string' });
+    })
+    .toThrowError(new TypeError('Bid parameter is not a number'));
+  })
 });
